6334c8b9351e1a2574e975e245d93ab2fd50bcbc
Rinke van den Berg <>
2019-05-30 23:05:01 +0200

    refactor

--- reload.sh
+++ reload.sh
@@ -1,5 +1,4 @@
 
-# tmux send-keys -t .1 "node vuel.js && osascript ~/apples/fast.scpt" ENTER
 tmux send-keys -t .1 c-c
-tmux send-keys -t .1 "node ../../vuel.js" ENTER
+tmux send-keys -t .1 "node ../.." ENTER
 
