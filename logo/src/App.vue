6562f5c9842faa8607722b606326b6c714b876b6
Rinke van den Berg <>
2019-09-30 07:16:50 +0200

    logo

---
+++ logo/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="app">
+        <img src="logo.svg" />
+    </div>
+</template>
+<script>
+
+export default {
+}
+
+</script>
+<style>
+
+body {
+    margin: 0;
+}
+
+#app {
+    svg {
+    width: 220px;
+    height: 190px;
+    }
+}
+
+</style>
---
+++ logo/src/index.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import App from './App';
+
+const app = new Vue({
+    render: h => h(App),
+}).$mount('#app');
